3ef973c3506454e7a8a5aff2e43f39ac79aaeabd
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-01-02 23:59:30 +0900

    java ApplicationProgramming main method

--- practice/ApplicationProgramming.java
+++ practice/ApplicationProgramming.java
@@ -1,4 +1,4 @@
-package demo.java.practice;
+package com.hooney.practice;
 
 import java.util.Scanner;
 
@@ -7,7 +7,12 @@ public class ApplicationProgramming {
 	public static void main(String[] args) {
 //		Practice01 pr01 = new Practice01();
 //		Practice02 pr02 = new Practice02();
-		Practice03 pr03 = new Practice03();
+//		Practice03 pr03 = new Practice03();
+//		pr03.testResult();
+//		Practice04 pr04 = new Practice04();
+//		pr04.birthStone();
+		Practice05 pr05 = new Practice05();
+		pr05.paperRockScissors();
 		
 		 
 
