756a428c280def34379a386589b42943826b05df
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-01-27 22:40:13 +0900

    Java-ApplicationProgramming

---
+++ bookmarket/User.java
@@ -0,0 +1,14 @@
+package bookmarket.welcome;
+
+public class User extends Person {
+
+	public User(String name, int phone, String addres) {
+		super(name, phone, addres);
+
+	}
+
+	public User(String name, int phone) {
+		super(name, phone);
+
+	}
+}
