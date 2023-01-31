6368693508444060c76a03a72f76712d3f5ed691
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-01-31 13:49:04 +0900

    Delete jv250 directory

--- jv250/jv250/src/main/java/com/kudangtang/banking/domain/BankConstants.java
+++
@@ -1,7 +0,0 @@
-package com.kudangtang.banking.domain;
-
-public class BankConstants {
-	public static final SavingsAccount SA = new SavingsAccount();
-	public static final CheckingAccount CA = new CheckingAccount();
-
-}
