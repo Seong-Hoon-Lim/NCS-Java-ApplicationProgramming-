37d20e3bb7b1cf832038031e97e427c75017a2a9
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-02-07 13:53:02 +0900

    Java-ApplicationProgramming

--- jv300/src/main/java/com/kudangtang/jv300/mod004/FormBasedHelloServlet.java
+++ jv300/src/main/java/com/kudangtang/jv300/mod004/FormBasedHelloServlet.java
@@ -10,15 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public class FormBasedHelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    private static final String DEFAULT_NAME = "World";       
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
-		String name = request.getParameter("userName");
-		if (name == null || name.length() == 0) {
-			name = DEFAULT_NAME;
-		}
-		response.setContentType("text/html; charset=UTF-8");
+    private static final String DEFAULT_NAME = "World";
+    
+    private void generateResponse(HttpServletRequest request, HttpServletResponse response) 
+    		throws ServletException, IOException{
+    	String name = request.getParameter("userName");
+    	//name.length() == 0 경우는 WAS 업체에서 null 값을 "" 로 하는 경우가
+    	//있으므로 유효성 검증을 진행
+    	if (name == null || name.length() == 0) {
+    		name = DEFAULT_NAME;
+    	}
+    	response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head><title></title></head>");
@@ -27,10 +29,16 @@ public class FormBasedHelloServlet extends HttpServlet {
 		out.println("</body>");
 		out.println("</html>");
 		out.close();
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		generateResponse(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
+		generateResponse(request, response);
 		
 	}
 
