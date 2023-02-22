0b745e40eaa4beae8d732cee784f295474b85103
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-02-22 12:27:19 +0900

    Java-ApplicationProgramming

--- jv350/src/main/java/com/kudangtang/banking/dao/AccountDao.java
+++ jv350/src/main/java/com/kudangtang/banking/dao/AccountDao.java
@@ -2,11 +2,16 @@ package com.kudangtang.banking.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.kudangtang.banking.DataSource;
 import com.kudangtang.banking.domain.Account;
+import com.kudangtang.banking.domain.BankConstants;
+import com.kudangtang.banking.domain.CheckingAccount;
+import com.kudangtang.banking.domain.SavingsAccount;
 
 public class AccountDao {
 	private DataSource dataSource;
@@ -24,17 +29,33 @@ public class AccountDao {
 	 * @param account
 	 */
 	public void addAccount(Account account) {
-		String sql = "INSERT TO Account(accountNum, balance, interestRate, overdraft, accountType, customerId)"
+		String sql = "INSERT INTO Account(accountNum, balance, interestRate, overdraft, accountType, customerId)"
 						+ "VALUES(?,?,?,?,?,?)";
-		try (Connection con = dataSource.getConnection();
-			 PreparedStatement pstmt = con.prepareStatement(sql);) {
-			 pstmt.setString(1, account.getAccountNum());
-			 pstmt.setDouble(2, account.getBalance());
-			 pstmt.setDouble(3, account.getInterestRate());
-			 pstmt.setDouble(4, account.getOverdraft());
-			 pstmt.setByte(5, (byte)account.getAccountType());
-			 pstmt.setLong(6, account.getCustomerId());
-			
+		try {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			try {
+				con = dataSource.getConnection();
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, account.getAccountNum());
+				pstmt.setDouble(2, account.getBalance());
+				
+				if (account instanceof SavingsAccount) {
+					pstmt.setDouble(3, ((SavingsAccount)account).getInterestRate());
+					pstmt.setDouble(4, 0.0);
+					pstmt.setString(5, String.valueOf(BankConstants.SA));
+				}
+				else {
+					pstmt.setDouble(3, 0.0);
+					pstmt.setDouble(4, ((CheckingAccount)account).getOverdraft());
+					pstmt.setString(5, String.valueOf(BankConstants.CA));
+				}
+				pstmt.setLong(6, account.getCustomer().getId());
+				pstmt.executeUpdate();
+			}
+			finally {
+				DataSource.close(pstmt, con);
+			}
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
@@ -48,7 +69,41 @@ public class AccountDao {
 	 */
 	public List<Account> findAllAccountList() {
 		// TODO Auto-generated method stub
-		return null;
+		String sql = "SELECT * FROM Account";
+		List<Account> list = new ArrayList<>();
+		Account account = null;
+		
+		try {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			try {
+				con = dataSource.getConnection();
+				pstmt = con.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				
+				while(rs.next()) {
+					account = new Account();
+					account.setAid(rs.getLong("aid"));
+					account.setAccountNum(rs.getString("accountNum"));
+					account.setBalance(rs.getDouble("balance"));
+					account.setInterestRate(rs.getDouble("interestRate"));
+					account.setOverdraft(rs.getDouble("overdraft"));
+					account.setAccountType((char) rs.getByte(("accountType")));
+					account.setAid(rs.getLong("aid"));
+					account.setAid(rs.getLong("aid"));
+					account.setAid(rs.getLong("aid"));
+					list.add(account);
+				}
+			}
+			finally {
+				DataSource.close(rs, pstmt, con);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 	/**
