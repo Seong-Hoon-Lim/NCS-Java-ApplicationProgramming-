fbb89aabbc515a4534a09bb1a8b7c655ed7b730d
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-02-25 23:43:55 +0900

    Delete jv300 directory

--- jv300/src/main/java/com/kudangtang/jv300/mod010/exception/ConnNotAvailException.java
+++
@@ -1,9 +0,0 @@
-package com.kudangtang.jv300.mod010.exception;
-
-public class ConnNotAvailException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public ConnNotAvailException(String msg) {
-		super(msg);
-	}
-}
